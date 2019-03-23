91f1da3a21e4ad334ba5b64fd4c480de60333b1d
顾咏 <zhijiang.nzj@alibaba-inc.com>
2019-03-23 17:03:28 +0800

    Update Apis

--- V20180118/Api.php
+++ V20180118/Api.php
@@ -2,7 +2,22 @@
 
 namespace AlibabaCloud\TeslaDam\V20180118;
 
+use AlibabaCloud\ApiResolverTrait;
 use AlibabaCloud\Rpc;
+
+/**
+ * Resolve Api based on the method name.
+ *
+ * @method Action action(array $options = [])
+ * @method ActionDiskRma actionDiskRma(array $options = [])
+ * @method ActionDiskCheck actionDiskCheck(array $options = [])
+ * @method ActionDiskMask actionDiskMask(array $options = [])
+ * @method HostGets hostGets(array $options = [])
+ */
+class TeslaDamApiResolver
+{
+    use ApiResolverTrait;
+}
 
 class V20180118Rpc extends Rpc
 {
